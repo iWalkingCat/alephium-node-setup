9cce939bc6241008923207ae70fb4e52c5958f45
TheWalkingCat <thewalkingcat900@gmail.com>
2024-11-04 10:42:18 +0200

    Update

--- hello.sh
+++ hello.sh
@@ -1,6 +1,12 @@
-apt update -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install vim tmux git curl 
 
+# Install Docker Engine and Docker Compose plugin:
 bash install-docker-and-compose.sh
 
-
+# Clone Alephium node to "/home/<user>/alephium-stack":
 git clone https://github.com/alephium/alephium-stack.git /home/$(whoami)/alephium-stack
+
+# Start Alephium node:
+cd /home/$(whoami)/alephium-stack/devnet
+docker-compose up -d
